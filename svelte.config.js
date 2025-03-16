48e6a53195aa3e60833f134cfdfd781c84a2fbed
joffutt-dev <joeoffutt4@gmail.com>
2025-03-15 20:53:08 -0400

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+
+// svelte.config.js
+ 
+import { preprocessMeltUI, sequence } from '@melt-ui/pp'
+ 
+const config = {
+  // ... other svelte config options
+  preprocess: sequence([
+    // ... other preprocessors
+    preprocessMeltUI() // add to the end!
+  ])
+}
+ 
+export default config
